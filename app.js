c87ebb889f301f9d61ba084be471e7004c5298ec
root <yhongtian@gmail.com>
2016-10-03 14:56:26 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+var angMod = angular.module("myApp", []);
+angMod.controller('meController', function($scope, $http) {
+            $scope.res = [];
+            $scope.submit = function(){
+               var data = {
+                  FirstName: $scope.firstname,
+                  LastName: $scope.lastname,
+                  Sex: $scope.sex,
+                  Street: $scope.street,
+                  City: $scope.city,
+                  State: $scope.state,
+                  Zipcode: $scope.zipcode,
+                  Email: $scope.email,
+                  DOB: $scope.birthday
+               };
+               
+               $scope.res.push(data);
+
+            }
+
+            $scope.cancel = function(){
+                  $scope.firstname = "";
+                  $scope.lastname = "";
+                  $scope.sex = "";
+                  $scope.street = "";
+                  $scope.city = "" ;
+                  $scope.state = "";
+                  $scope.zipcode = "";
+                  $scope.email = "";
+                  $scope.birthday = "";
+
+            }
+            
+         });
